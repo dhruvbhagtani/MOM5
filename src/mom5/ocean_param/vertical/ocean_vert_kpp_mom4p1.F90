822bf42c9f02b6bb86d5394374aaa21ef73d1630
Dhruv Bhagtani <dhruvbhagtani@gmail.com>
2021-05-26 12:19:54 +0530

    Parametrization for resolved velocity shear

--- src/mom5/ocean_param/vertical/ocean_vert_kpp_mom4p1.F90
+++ src/mom5/ocean_param/vertical/ocean_vert_kpp_mom4p1.F90
@@ -241,6 +241,16 @@ module ocean_vert_kpp_mom4p1_mod
 !  compatibility, we default to smooth_ri_kmax_eq_kmu=.false. 
 !  </DATA> 
 !
+!  <DATA NAME="dVsq_Dhruv_param" TYPE="logical">
+!  When dVsq_Dhruv_param = .true., the resolved velocity shear (dVsq) 
+!  is calculated using an analytical parametrisation, instead of the 
+!  plain definition. Testing to implement in nostress models where
+!  we need KPP mixing layer to be independent of any changes in
+!  momentum equations. 
+!  </DATA> 
+!
+
+!
 !</NAMELIST>
 
 use constants_mod,    only: epsln, pi
@@ -425,6 +435,7 @@ logical :: wsfc_combine_runoff_calve = .true.  ! for combining runoff+calving to
 logical :: bvf_from_below    = .false.   ! Use BV-freq. at the cell bottom instead of the cell top (Danabasoglu et al.) 
 logical :: variable_vtc      = .false.   ! Make vtc dependent on BV-freq.
 logical :: use_max_shear     = .false.   ! Use maximum shear instead of 4-point average
+logical :: dVsq_Dhruv_param  = .false.   ! Use analytical parameterization for resolved velocity shear
 logical :: linear_hbl        = .true.    ! To use the linear interpolation as Large etal (1994).
                                          ! Set to .false. for quadratic interpolation as in Danabasoglu et al.
 logical :: calc_visc_on_cgrid=.false.    ! calculate viscosity directly on c-grid
@@ -508,7 +519,7 @@ logical :: module_is_initialized = .FALSE.
 logical :: debug_this_module     = .FALSE.
 
 namelist /ocean_vert_kpp_mom4p1_nml/ use_this_module, shear_instability, double_diffusion,  &
-                                     diff_cbt_iw, visc_cbu_iw,                              &
+                                     diff_cbt_iw, visc_cbu_iw, dVsq_Dhruv_param             &
                                      visc_cbu_limit, diff_cbt_limit,                        &
                                      visc_con_limit, diff_con_limit,                        &
                                      concv, Ricr, non_local_kpp, smooth_blmc,               &
@@ -657,7 +668,12 @@ ierr = check_nml_error(io_status,'ocean_vert_kpp_mom4p1_nml')
   else
     write(stdoutunit,'(1x,a)') '==> NOTE from ocean_vert_kpp_mom4p1_mod: '// &
     'Use average shear around a tracer cell for diagnostics of hbl.'  
-  endif                               
+  endif
+  if(dVsq_Dhruv_param) then
+    write(stdoutunit,'(1x,a)') '==> NOTE from ocean_vert_kpp_mom4p1_mod: Use explicit parameterization for dVsq'
+  else
+    write(stdoutunit,'(1x,a)') '==> NOTE from ocean_vert_kpp_mom4p1_mod: Use velocity from momentum equations for dVsq'
+  endif
   if (linear_hbl) then
     write(stdoutunit,'(1x,a)') '==> NOTE from ocean_vert_kpp_mom4p1_mod: Use linear interpolation to find hbl'
   else
@@ -1068,32 +1084,44 @@ subroutine vert_mix_kpp_mom4p1 (aidif, Time, Thickness, Velocity, T_prog, T_diag
 !-----------------------------------------------------------------------
 !     compute vertical difference of velocity squared
 !-----------------------------------------------------------------------
-
-    do k=1,nk
-      do j=jsd,jed
-        do i=isd,ied
-            wrk2(i,j,k) = (Velocity%u(i,j,1,1,tau) - Velocity%u(i,j,k,1,tau))**2   &
-                        + (Velocity%u(i,j,1,2,tau) - Velocity%u(i,j,k,2,tau))**2
-        enddo
-      enddo
-    enddo
-    if (use_max_shear) then
-      do k=1,nk
-        do j=jsc,jec
-          do i=isc,iec
-             dVsq(i,j,k) = max(wrk2(i,j,k),wrk2(i-1,j,k),wrk2(i,j-1,k),wrk2(i-1,j-1,k))
+    if(dVsq_Dhruv_param) then
+      do k = 1, nk
+        do j = jsd, jed
+          do i = isd, ied
+            dVsq(i,j,k) = exp(-1e-4 * Thickness%depth_zt(i,j,k)/Velocity%ustar(i,j)**0.5)
+            dVsq(i,j,k) = dVsq(i,j,k) * (Velocity%ustar(i,j) + 80*Velocity%ustar(i,j)**2)
+            dVsq(i,j,k) = dVsq(i,j,k) * (Thickness%depth_zt(i,j,k)/60)
           enddo
         enddo
       enddo
+
     else
       do k=1,nk
-        do j=jsc,jec
-          do i=isc,iec
-             active_cells = Grd%umask(i,j,k) + Grd%umask(i-1,j,k) + Grd%umask(i,j-1,k) + Grd%umask(i-1,j-1,k) + epsln
-             dVsq(i,j,k)  = (wrk2(i,j,k) + wrk2(i-1,j,k) + wrk2(i,j-1,k) + wrk2(i-1,j-1,k))/active_cells
+        do j=jsd,jed
+          do i=isd,ied
+              wrk2(i,j,k) = (Velocity%u(i,j,1,1,tau) - Velocity%u(i,j,k,1,tau))**2   &
+                          + (Velocity%u(i,j,1,2,tau) - Velocity%u(i,j,k,2,tau))**2
           enddo
         enddo
       enddo
+      if (use_max_shear) then
+        do k=1,nk
+          do j=jsc,jec
+            do i=isc,iec
+               dVsq(i,j,k) = max(wrk2(i,j,k),wrk2(i-1,j,k),wrk2(i,j-1,k),wrk2(i-1,j-1,k))
+            enddo
+          enddo
+        enddo
+      else
+        do k=1,nk
+          do j=jsc,jec
+            do i=isc,iec
+               active_cells = Grd%umask(i,j,k) + Grd%umask(i-1,j,k) + Grd%umask(i,j-1,k) + Grd%umask(i-1,j-1,k) + epsln
+               dVsq(i,j,k)  = (wrk2(i,j,k) + wrk2(i-1,j,k) + wrk2(i,j-1,k) + wrk2(i-1,j-1,k))/active_cells
+            enddo
+          enddo
+        enddo
+      endif
     endif
 
     if(id_dVsq > 0) call diagnose_3d_u(Time, Grd, id_dVsq, dVsq(:,:,:)) 

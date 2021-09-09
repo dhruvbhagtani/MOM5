d1a48de4a9df79eb54dd06fa06d5abba180c5a2a
Dhruv Bhagtani <dhruvbhagtani@gmail.com>
2021-09-09 16:00:53 +0530

    Corrected piece of code where wind_mask was defined before declaration

--- src/mom5/ocean_core/ocean_model.F90
+++ src/mom5/ocean_core/ocean_model.F90
@@ -1178,7 +1178,6 @@ subroutine ocean_model_init(Ocean, Ocean_state, Time_init, Time_in, &
     call ocean_workspace_init(Domain, Grid)
     call set_ocean_hgrid_arrays(Domain, Grid)
     call ocean_topog_init(Domain, Grid, 'INPUT/grid_spec.nc', vert_coordinate_type)
-    call wind_mask_input(Domain, Velocity)
     call ocean_obc_init(have_obc, Time_steps, Domain, Grid, Ocean_options, &
           vert_coordinate, debug=debug)
     call set_ocean_vgrid_arrays(Domain, Grid, have_obc)
@@ -1267,6 +1266,7 @@ subroutine ocean_model_init(Ocean, Ocean_state, Time_init, Time_in, &
                               vert_coordinate, vert_coordinate_class, vert_coordinate_type, &
                               use_blobs, introduce_blobs, dtime_t, debug=debug)
     call ocean_operators_init(Grid, Domain, Thickness, horz_grid)
+    call wind_mask_input(Domain, Velocity)
 
     ! initialize prognostic tracers 
     T_prog => ocean_prog_tracer_init(Grid, Thickness, Ocean_options, Domain, Time, Time_steps, &
